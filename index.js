0775ce458b822782064bdb3b7a61427977df2b4b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 01:50:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+class Cat {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak(){
+    return `${this.name} says meow!`;
+  }
+}
+ 
+class Dog {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+}
+ 
+class Bird {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+}
